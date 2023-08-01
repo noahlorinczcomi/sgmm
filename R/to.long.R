feae39c1a9247a4486dcb825ac4388f5ba9c5926
Noah Lorincz-Comi <39975632+noahlorinczcomi@users.noreply.github.com>
2023-08-01 14:49:30 -0400

    Update to.long.R

--- R/to.long.R
+++ R/to.long.R
@@ -17,11 +17,7 @@ to.long <- function(data, id_column, time_columns, date_columns) {
   ordered_id_levels <- as.numeric(as.character(factored_ids))
   # we don't want to use the actual date columns, but instead the month columns
   # corresponding to the date columns. that is what is happening here:
-  names_list <- c("m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10",
-                  "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18", "m19", "m20",
-                  "m21", "m22", "m23", "m24", "m25", "m26", "m27", "m28", "m29", "m30",
-                  "m31", "m32", "m33", "m34", "m35", "m36", "m37", "m38", "m39", "m40",
-                  "m41", "m42", "m43", "m44", "m45", "m46", "m47", "m48", "m49", "m50")
+  names_list=paste0('m',1:100)
   month_columns <- names_list[1:length(date_columns)]
   # new_empty df
   new_df <- data.frame()
