758e1c814ee4c1c6c39345bdc9021bdec0d55736
Noah Lorincz-Comi <39975632+noahlorinczcomi@users.noreply.github.com>
2023-08-01 14:48:42 -0400

    Update m.since.assign.R

--- R/m.since.assign.R
+++ R/m.since.assign.R
@@ -19,11 +19,7 @@ m.since.assign <- function(data, date_list, to='months') {
     my_bind <- cbind(my_bind, month_values)
   }
   # naming columns
-  names_list <- c("m1", "m2", "m3", "m4", "m5", "m6", "m7", "m8", "m9", "m10",
-                  "m11", "m12", "m13", "m14", "m15", "m16", "m17", "m18", "m19", "m20",
-                  "m21", "m22", "m23", "m24", "m25", "m26", "m27", "m28", "m29", "m30",
-                  "m31", "m32", "m33", "m34", "m35", "m36", "m37", "m38", "m39", "m40",
-                  "m41", "m42", "m43", "m44", "m45", "m46", "m47", "m48", "m49", "m50")
+  names_list=paste0('m',1:100)
   colnames(my_bind) <- names_list[1:dim(my_bind)[2]]
   # attach to new df
   new_df <- cbind(data, my_bind)
